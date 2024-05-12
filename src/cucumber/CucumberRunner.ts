f9580506860b7c1b3ac41c982420d86a0a1e7f20
mandziak-nv <mandziak.nv@gmail.com>
2024-05-12 23:58:05 +0300

    update CucumberRunner

---
+++ src/cucumber/CucumberRunner.ts
@@ -0,0 +1,69 @@
+import * as vscode from 'vscode';
+import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
+import { getExtensionConfiguration } from '../configuration/getExtensionConfiguration';
+
+export class CucumberRunner {
+    private static cucumberProcess: ChildProcessWithoutNullStreams | undefined;
+
+    public static async runTest(testRun: vscode.TestRun, scenarioName: string, debug?: boolean): Promise<string[]> {
+        this.killCucumberProcess();
+
+        return new Promise((resolve) => {
+            const cucumberOutput: string[] = [];
+
+            this.cucumberProcess = this.spawnCucumberProcess(testRun, scenarioName, debug);
+
+            this.cucumberProcess.stdout.on('data', (chunk: any) => {
+                this.log(testRun, chunk);
+                cucumberOutput.push(chunk.toString());
+            });
+
+            this.cucumberProcess.stderr.on('data', (chunk: any) => {
+                this.log(testRun, chunk);
+            });
+
+            this.cucumberProcess.on('close', (code: number | null, signal: NodeJS.Signals | null) => {
+                resolve(cucumberOutput);
+            });
+        });
+    }
+
+    private static spawnCucumberProcess(testRun: vscode.TestRun, scenarioName: string, debug?: boolean): ChildProcessWithoutNullStreams {
+        const cwd = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders[0].uri.path;
+        const cucumberPath = './node_modules/.bin/cucumber-js';
+        const scenarioNameRegexed = `^${scenarioName.replace(/([.+*?^$()[\]{}|\\])/g, '\\$1')}$`;
+        const { featurePaths, env, cliOptions } = getExtensionConfiguration();
+        const nodeArguments = [cucumberPath, ...featurePaths, '--name', scenarioNameRegexed, ...cliOptions];
+
+        this.log(testRun,
+            'Executing command: '
+            // + (debug ? 'DEBUG=cucumber ' : '')
+            + (Object.keys(env).length ? `${Object.keys(env).map(vr => vr + '=......').join(' ')} ` : '')
+            + 'node '
+            + [cucumberPath, ...featurePaths, '--name', `"${scenarioNameRegexed}"`, ...cliOptions].join(' ')
+            + '\n\n'
+        );
+
+        return spawn(
+            'node', nodeArguments,
+            {
+                cwd: cwd,
+                env: {
+                    ...process.env, ...env,
+                    // ...(debug && { 'DEBUG': 'cucumber' }),
+                },
+            }
+        );
+    }
+
+    public static killCucumberProcess(signal?: NodeJS.Signals | number): boolean {
+        if (this.cucumberProcess && !this.cucumberProcess.killed) {
+            return this.cucumberProcess.kill(signal);
+        }
+        return true;
+    }
+
+    public static log(testRun: vscode.TestRun, message: any): void {
+        testRun.appendOutput(message.toString().replaceAll('\n', '\r\n'));
+    }
+}
