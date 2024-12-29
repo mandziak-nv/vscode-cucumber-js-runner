daa24a1f3603cbf231ba3fe528201f21afd457cd
Aleksandr M <netstroix@gmail.com>
2024-12-29 10:50:43 +0800

    Refactor getExtensionConfiguration to enhance 'cwd' handling by replacing ${workspaceFolder} variable with the actual workspace path, ensuring correct current working directory setup.

--- src/configuration/getExtensionConfiguration.ts
+++ src/configuration/getExtensionConfiguration.ts
@@ -2,10 +2,12 @@ import * as vscode from "vscode";
 
 export function getExtensionConfiguration(): { featurePaths: string[], env: { [key: string]: string }, cliOptions: string[], cucumberPath: string, cwd: string  } {
     const configuration = vscode.workspace.getConfiguration('cucumber_runner');
+    const workspaceFolder = vscode.workspace.workspaceFolders?.[0].uri.fsPath!;
+    const cwd = configuration.get<string>('cwd')!.trim().length > 0 ? configuration.get<string>('cwd')!.replace(/\$\{workspaceFolder\}/g, workspaceFolder) : workspaceFolder;
     return {
         featurePaths: configuration.get<string[]>('features')!,
         env: configuration.get<{ [key: string]: string }>('env_variables')!,
-        cwd: configuration.get<string>('cwd') ?? vscode.workspace.workspaceFolders?.[0].uri.fsPath!,
+        cwd: cwd,
         cliOptions: configuration.get<string[]>('cli_options')!,
         cucumberPath: configuration.get<string>('cucumber_path')!,
     }
